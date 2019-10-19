2dbbf992593c6fce4992ab629e879d7c38ddba7b
AcnoZed <Me.ouss.am@outlook.de>
2019-10-19 21:17:02 +0200

    Product-Klasse hinzugefuegt

---
+++ src/main/java/acno/projects/CheckOut/Product.java
@@ -0,0 +1,13 @@
+package acno.projects.CheckOut;
+
+import lombok.Data;
+
+@Data
+public class Product {
+    private String produktName = "ButterMilch";
+    private int produktID = 128;
+    private double preis = 0.59;
+
+
+
+}
